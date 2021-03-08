9c9642c7a7422659a22de279478f328e370b2cab
phatg <75820154+phatg@users.noreply.github.com>
2021-03-08 10:28:31 -0800

    Update how-to-learn.js

--- week1/d1-expressions/code-it-out/how-to-learn.js
+++ week1/d1-expressions/code-it-out/how-to-learn.js
@@ -3,16 +3,17 @@ What are the two 'types' of learning we alternate
 between here at AppAcademy? What are three examples 
 of each?
 
-1. 
-  1. 
-  2. 
-  3. 
-2. 
-  1.
-  2.
-  3.
+1. Active learning 
+  1. Allow us to engage with the material.
+  2. Give us space to receive feedback on our progress.
+  3. Feel difficult.
+2. Passive learning
+  1. Do not let us engage the material.
+  2. Do not have any source of feedback.
+  3. Feel relatively easy.
 
 What is Desirable Difficulty?
-
-
+- When effective learners desire challenge.
+  
 */
+
