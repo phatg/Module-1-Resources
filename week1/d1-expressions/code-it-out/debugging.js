ab2d5ac65f9172bea74d52edcdbcc6a5aac35fb4
phatg <75820154+phatg@users.noreply.github.com>
2021-03-08 10:40:51 -0800

    Update debugging.js

--- week1/d1-expressions/code-it-out/debugging.js
+++ week1/d1-expressions/code-it-out/debugging.js
@@ -1,8 +1,8 @@
 /* 
 What is Debugging?
-
-
+- Process of finding out the error and try to solve it.
+- Just like being a detective, gather each information and solve it.
 How often will you be wrong, and need to debug?
-
+- A lot.
 */
 
