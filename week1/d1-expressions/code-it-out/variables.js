fc8853b2d41b0cb833b5ed7a44d174747c8f0aa8
phatg <75820154+phatg@users.noreply.github.com>
2021-03-08 13:11:22 -0800

    Update variables.js

--- week1/d1-expressions/code-it-out/variables.js
+++ week1/d1-expressions/code-it-out/variables.js
@@ -10,18 +10,18 @@ let location = 'New York';
 /* 
 The line above is an example of Variable...
   A: Declaration
-  B: Initialization
+  B: Initialization B
   C: Assignment
 
 */
-console.log(location); // prints:
+console.log(location); // prints: New York
 console.log(location[1]); // prints: 'e' <- More on this TOMORROW
 
 /***** Problem 2 *****/
 let location;
 /* 
 The line above is an example of Variable...
-  A: Declaration
+  A: Declaration A
   B: Initialization
   C: Assignment
 */
@@ -31,27 +31,27 @@ location = 'SF';
 The line above is an example of Variable...
   A: Declaration
   B: Initialization
-  C: Assignment
+  C: Assignment C
 
 */
 
-console.log(location); // prints:
+console.log(location); // prints: SF
 /***** Problem 3 *****/
 
 let age = 0;
 age + 5; // no assignment used
-console.log(age); // prints:
+console.log(age); // prints: 0
 age = age + 5; // assignment is used
-console.log(age); // prints:
+console.log(age); // prints: 5
 age += 5; // assignment is used, this is shorthand
-console.log(age); // prints:
+console.log(age); // prints: 5
 age++; // assignment is used, increment
-console.log(age); // prints:
+console.log(age); // prints: 1
 age--; // assignment is used, decrement
-console.log(age); // prints:
+console.log(age); // prints: -1
 
 /***** Problem 4 *****/
 let food;
-console.log(food); // prints:
-console.log(food + 4); // prints:
-console.log('2' + 2); // prints:
+console.log(food); // prints: undefined
+console.log(food + 4); // prints: NaN
+console.log('2' + 2); // prints: 22
