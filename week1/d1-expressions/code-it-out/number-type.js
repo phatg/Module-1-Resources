13766c68c6f626ab8b590d486442bde9e9fa6005
phatg <75820154+phatg@users.noreply.github.com>
2021-03-08 11:21:18 -0800

    Update number-type.js

--- week1/d1-expressions/code-it-out/number-type.js
+++ week1/d1-expressions/code-it-out/number-type.js
@@ -6,17 +6,17 @@ For each example, write what will print to the console
 */
 /***** Problem 1 *****/
 
-console.log(1 + 1); // prints:
-console.log(2.5 - 2); // prints:
-console.log(3 * 4); // prints:
-console.log(99 / 9); // prints:
-console.log(9 / 2); // prints:
-console.log(-3 * 3); // prints:
+console.log(1 + 1); // prints: 2
+console.log(2.5 - 2); // prints: 0.5
+console.log(3 * 4); // prints: 12
+console.log(99 / 9); // prints: 11
+console.log(9 / 2); // prints: 4.5
+console.log(-3 * 3); // prints: -9
 
 /***** Problem 2 *****/
 
-console.log(3 + 2 * 4); // prints:
-console.log((3 + 2) * 4); // prints:
+console.log(3 + 2 * 4); // prints: 11
+console.log((3 + 2) * 4); // prints: 20
 /* 
 Why does this behavior occur?
 Hint: PEMDAS
@@ -24,27 +24,33 @@ Hint: PEMDAS
 */
 
 /***** Problem 3 *****/
-console.log(22 % 5); // prints:
-console.log(23 % 5); // prints:
-console.log(24 % 5); // prints:
-console.log(25 % 5); // prints:
-console.log(26 % 5); // prints:
+console.log(22 % 5); // prints: 2
+console.log(23 % 5); // prints: 3
+console.log(24 % 5); // prints: 4
+console.log(25 % 5); // prints: 0
+console.log(26 % 5); // prints: 1
 
 /* 
 What is this "%" symbol called, and what does it do?
-
+-  This '%' symbol called modulo, it 
 
 */
 /***** Problem 4 *****/
-console.log(10 % 2); // prints:
-console.log(10 % 3); // prints:
-console.log(4 % 10); // prints:
+console.log(10 % 2); // prints: 0
+console.log(10 % 3); // prints: 1
+console.log(4 % 10); // prints: 4
 
 /**** Bonus ****/
 /* 
 How could you write an mathematical expression to 
 check and see if a number is even?
 
+let num = 10
+if ( num % 2 == 0 ) {
+  console.log('Even number'); print: Even number
+} else {
+   console.log('Odd number');
+}
 How would you know if it's even or odd?
-
+Depends on what number the user sets on the variable. If the user sets number 9, the result will come out to be out. 
 */
