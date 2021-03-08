8991aa33fda3a483fb1bedc4fae7e092fd7fc4f3
phatg <75820154+phatg@users.noreply.github.com>
2021-03-08 11:07:55 -0800

    Update boolean-type.js

--- week1/d1-expressions/code-it-out/boolean-type.js
+++ week1/d1-expressions/code-it-out/boolean-type.js
@@ -4,15 +4,15 @@ Instructions:
 For each example, write what will print to the console
 
 */
-console.log(!true); // prints:
-console.log(!!true); // prints:
+console.log(!true); // prints: false
+console.log(!!true); // prints: true
 
-console.log(false && false); // prints:
-console.log(false && true); // prints:
-console.log(true && true); // prints:
+console.log(false && false); // prints: false
+console.log(false && true); // prints: false
+console.log(true && true); // prints: true
 
-console.log(false || false); // prints:
-console.log(false || true); // prints:
-console.log(true || true); // prints:
+console.log(false || false); // prints: false
+console.log(false || true); // prints: true
+console.log(true || true); // prints: true
 
-console.log((!false && true) || !(false && true)); // prints:
+console.log((!false && true) || !(false && true)); // prints: true
